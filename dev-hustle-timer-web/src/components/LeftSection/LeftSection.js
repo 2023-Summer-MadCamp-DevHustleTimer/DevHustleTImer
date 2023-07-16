384b94de7c7be24673629aedcf5e671f257514ad
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-07-17 00:19:44 +0900

    feat: 플레이리스트 거의 구현 1. 상단에 유튜브 영상 재생 2. draggable 플레이리스트 구현
    하단 링크 입력바 만들면 일단 끝

--- dev-hustle-timer-web/src/components/LeftSection/LeftSection.js
+++ dev-hustle-timer-web/src/components/LeftSection/LeftSection.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import moment from "moment";
 import "./LeftSection.css";
 
+const divmod = (x, y) => [Math.floor(x / y), x % y];
+
 const LeftSection = () => {
+  const eventTime = moment("2023-07-19 21:00:00");
+  const currentTime = moment();
+  const timeRemaining = moment.duration(eventTime.diff(currentTime)).asSeconds();
+  const [tmp1, remainingSeconds] = divmod(timeRemaining, 60);
+  const [tmp2, remainingMinutes] = divmod(tmp1, 60);
+  const [remainingDays, remainingHours] = divmod(tmp2, 24);
+
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+
   return (
     <div className="left_section">
-      <div class="notice">
+      <div className="notice">
         <span>내일 스크럼은 14:00 입니다</span>
         <br />
         <span>지각하지 마세요!</span>
       </div>
-      <div class="timer">
-        <span class="goal">발표까지</span>
+      <div className="timer">
+        <span className="goal">발표까지</span>
         <br />
-        <span class="time-left">6일 15시간 23초</span>   
+        <span className="time-left">
+          {remainingDays}일 {remainingHours}시간 {remainingMinutes}분 {parseInt(remainingSeconds)}초
+        </span>
       </div>
-      <span  class="queto">
+      <span  className="queto">
         It doesn’t work...... why?
         <br />
         It works..................... why?
--- dev-hustle-timer-web/src/components/RightSection/RightSection.js
+++ dev-hustle-timer-web/src/components/RightSection/RightSection.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./RightSection.css";
+import Music from "../Music/Music";
 
 const RightSection = () => {
+  console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
   return (
     <div className="right_section">
       <div className="navigator">
@@ -9,6 +11,7 @@ const RightSection = () => {
       </div>
       <div className="right_content">
         right_content
+        <Music></Music>
       </div>
     </div>
   );
