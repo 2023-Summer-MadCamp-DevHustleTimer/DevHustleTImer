310441fa801a5e82c7f50b86ff11d336256393ea
skykhs3 <skykhs3@naver.com>
2023-07-18 08:55:37 +0900

    Feat(front) : chat 최신 인거 밑에서부터 나오게함

--- dev-hustle-timer-web/src/components/Chat/Chat.js
+++ dev-hustle-timer-web/src/components/Chat/Chat.js
@@ -1,34 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./Chat.css";
 import axios from "axios";
 import io from "socket.io-client";
 
 const Chat = () => {
-
   const [message, setMessage] = useState("");
-  const [messageList, setMessageList] = useState([],);
+  const [messageList, setMessageList] = useState([]);
+  const scrollRef = useRef(null);
 
   useEffect(() => {
-
     const socket = io("http://localhost:8080");
-    getMessages();
+    getMessages(true);
+    
 
     socket.on("message", (msg) => {
       console.log("new messages");
-      getMessages();
+      getMessages(false);
     });
 
     return () => {
       socket.disconnect();
     };
-
   }, []);
 
-  async function getMessages() {
+  async function getMessages(tmp) {
     try {
-      var response = await axios.get('http://localhost:3001/api/me');
-      response = await axios.get('http://localhost:3001/api/message');
+      var response = await axios.get("http://localhost:3001/api/me");
+      response = await axios.get("http://localhost:3001/api/message");
       setMessageList(response.data);
+      if(tmp){
+        scrollToBottom(); 
+      }
+      // 스크롤을 가장 하단으로 이동
     } catch (error) {
       console.log(error);
     }
@@ -38,53 +41,62 @@ const Chat = () => {
     // 메시지를 전송하는 로직이 이곳에 들어갑니다.
     console.log(message);
     try {
-      let response = await axios.get('http://localhost:3001/api/me');
-      await axios.post('http://localhost:3001/api/message', {
+      let response = await axios.get("http://localhost:3001/api/me");
+      await axios.post("http://localhost:3001/api/message", {
         text: message,
-      })
-
+      });
     } catch (error) {
-      console.error("error")
+      console.error("error");
       //  console.log(error.response.data);
     }
 
     setMessage("");
-  }
+  };
 
   const handleChange = async (e) => {
     setMessage(e.target.value);
-
-    // test
-
-  }
+  };
 
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
+    if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault(); // 새 줄을 만드는 동작을 막습니다.
-      handleSend();       // 메시지를 전송합니다.
+      handleSend(); // 메시지를 전송합니다.
     }
-  }
+  };
+
+  const scrollToBottom = () => {
+    if (scrollRef.current) {
+      //움직이는 방법 다시 연구해보자.
+      scrollRef.current.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    }
+  };
+
   const renderChatBubbles = () => {
     let reversedMessageList = [];
     for (let i = messageList.length - 1; i >= 0; i--) {
       reversedMessageList.push(messageList[i]);
     }
 
-
     return reversedMessageList.map(function (data, index) {
-      var lr = (data.me == true ? 'chat-right' : 'chat-left');
+      var lr = data.me ? "chat-right" : "chat-left";
       return (
         <div className={`chat-bubble ${lr}`} key={index}>
           {data.text}
-        </div>);
+        </div>
+      );
     });
   };
+
   return (
-    <div class="cchat">
-      <div className="show-text">
-        {renderChatBubbles()}
-
-
+    <div className="cchat">
+      <div className="show-text" >
+      
+        <div className="show-text-inner" >
+         {/* 스크롤 위치로 이동시킬 요소 */}
+          {renderChatBubbles()}
+          
+        </div>
+        <div ref={scrollRef} />
       </div>
       <div className="input-text">
         <textarea
@@ -93,20 +105,12 @@ const Chat = () => {
           rows={4}
           value={message}
           onChange={handleChange}
-
           onKeyPress={handleKeyPress}
-
-        // onChange={(e) => setMessage(e.target.value)}
-        // onKeyPress={(e) => e.key === 'Enter' ? handleSend() : null}
         />
         <input type="submit" style={{ display: "none" }} />
-
-
       </div>
-
     </div>
-
   );
-}
+};
 
 export default Chat;
