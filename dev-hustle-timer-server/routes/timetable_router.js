e4c816e15fe397ecfe20e0e39ed194e8bd83c32a
skykhs3 <skykhs3@naver.com>
2023-07-18 14:53:32 +0900

    Feat(back) : 미참여자 알려줌
    notPrtc 라는 필드로 미참여자 알 수 있음.
    prtc라는 필드로 참여자 알 수 있음

--- dev-hustle-timer-server/routes/timetable_router.js
+++ dev-hustle-timer-server/routes/timetable_router.js
@@ -15,14 +15,24 @@ router.get('/', async function (req, res, next) {
             else {
                 const users = await User.findAll({ where: { eventId: user.eventId } });
                 const totals = new Array(24 * 2 * 7).fill(0);
-                
+                var notPrtc = [];
+                var prtc=[]
                 for(i=0;i<users.length;i++){
+                    let flag=0;
                     console.log(users[i].possibleTime);
                     for(j=0;j<336;j++){
                         totals[j]+=parseInt(users[i].possibleTime[j]);
+                        if(parseInt(users[i].possibleTime[j])==1)
+                            flag=1;
+                    }
+                    if(flag==0){
+                        notPrtc.push(users[i]);
+                    }
+                    else{
+                        prtc.push(users[i]);
                     }
                 }
-                res.json({totalNum:users.length,totalTime:totals});
+                res.json({totalNum:users.length,totalTime:totals,notPrtc:notPrtc,prtc:prtc});
             }
         } catch (error) {
             console.error(error);
